1c5e0e146721b777caeb1558ab3bacb0380a021c
Yun <ghini7170@gmail.com>
2023-08-21 14:54:39 +0900

    updated

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
         <body className={inter.className}>
           <div className='Navbar'>
             <button onClick={()=>{location.replace('http://localhost:3000/')}} style={{textDecorationLine:'none', border:'none', background:'transparent'}} className='Title_color'>   WAVE</button>
-            <button onClick={()=>{location.replace('http://localhost:3000/signup')}} className='NavBtn' style={{ textDecorationLine:'none'}}>LOGIN</button>
+            <button onClick={()=>{location.replace('http://localhost:3000/signup')}} className='NavBtn' style={{ textDecorationLine:'none', marginLeft:'67%'}}>SIGN IN</button>
             <button onClick={()=>{location.replace('http://localhost:3000/contact')}} className='NavBtn' style={{textDecorationLine:'none'}}>CONTACT</button>
           </div>
           {children}
--- app/page.js
+++ app/page.js
@@ -5,16 +5,60 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className='SideDiv' style={{paddingTop:'22%'}}>
-    <button onClick={()=>{location.replace('http://localhost:3000/find')}} className='Find_Btn'
-    style={{marginLeft:'25%'}}>동아리 찾아보기</button>
-    <div className='InnerDiv' style={{paddingTop:'34%', marginLeft:'65%'}}>
+    <div  style={{marginTop:'2%'}}>
+      <AD_Banner/>
+      <div className='SideDiv' style={{marginTop:'-70%'}}>
+      <button onClick={()=>{location.replace('http://localhost:3000/find')}} className='Find_Btn'
+      style={{marginLeft:'35%'}}>동아리 찾아보기</button>
+      <div className='InnerDiv' style={{marginTop:'3.8%', marginLeft:'67%'}}>
       <button onClick={()=>{location.replace('http://localhost:3000/rcr')}} className='Recruit_Btn'>모집하기</button>
       <div><h4></h4></div>
       <button className='Register_Btn' onClick={()=>{location.replace('http://localhost:3000/upload')}}>등록/관리</button>
     </div>
+      </div>
   </div>
   )
 }
 
 
+function AD_Banner(){
+  return (
+    <div>
+        <section class="ADBANNER" aria-label="Gallery">
+        <ol class="viewport">
+            <li id="slide1" tabindex="1" class="slide">
+            <div class="snapper">
+                <a href="#slide3" class="prev"></a>
+                <a href="#slide2" class="next"></a>
+            </div>
+            </li>
+            <li id="slide2" tabindex="2" class="slide">
+            <div class="snapper">
+                <a href="#slide1" class="prev"></a>
+                <a href="#slide3" class="next"></a>
+            </div>
+            </li>
+            <li id="slide3" tabindex="3" class="slide">
+            <div class="snapper">
+                <a href="#slide2" class="prev"></a>
+                <a href="#slide1" class="next"></a>
+            </div>
+            </li>
+        </ol>
+        </section>
+        <aside class="navigation">
+    <ol class="navigation-list">
+    <li class="navigation-item">
+        <a href="#slide1" class="navigation-button">Go to slide 1</a>
+    </li>
+    <li class="navigation-item">
+        <a href="#slide2" class="navigation-button">Go to slide 2</a>
+    </li>
+    <li class="navigation-item">
+        <a href="#slide3" class="navigation-button">Go to slide 3</a>
+    </li>
+    </ol>
+  </aside>
+        </div>
+  )
+}
