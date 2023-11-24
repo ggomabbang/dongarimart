8388c7871fb25072568924b4455d30e8c37d47d8
ggomabbang <badawa7@naver.com>
2023-11-25 00:29:00 +0900

    database ver.0.1.0 - 깃 정리 및 데이터베이스 정비

--- prisma/migrations/20231011044918_fix2/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `isRecruiting` on the `clublist` table. The data in that column could be lost. The data in that column will be cast from `Int` to `TinyInt`.
-
-*/
--- AlterTable
-ALTER TABLE `clublist` MODIFY `isRecruiting` BOOLEAN NOT NULL DEFAULT false;
---
+++ prisma/migrations/20231124150733_ver_0_1_0/migration.sql
@@ -0,0 +1,149 @@
+-- CreateTable
+CREATE TABLE `ClubList` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `clubName` VARCHAR(20) NOT NULL,
+    `classification` VARCHAR(20) NOT NULL,
+    `oneLine` VARCHAR(100) NOT NULL,
+    `short` VARCHAR(500) NOT NULL,
+    `isRecruiting` BOOLEAN NOT NULL DEFAULT false,
+    `recruitStart` DATE NULL,
+    `recruitEnd` DATE NULL,
+    `recruitTarget` VARCHAR(100) NULL,
+    `pageURL` VARCHAR(256) NULL,
+    `image` VARCHAR(256) NULL,
+    `view` INTEGER UNSIGNED NOT NULL,
+    `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    UNIQUE INDEX `ClubList_clubName_key`(`clubName`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `TagList` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `tagName` VARCHAR(20) NOT NULL,
+    `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE INDEX `TagList_tagName_key`(`tagName`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ClubTag` (
+    `clubId` INTEGER UNSIGNED NOT NULL,
+    `tagId` INTEGER UNSIGNED NOT NULL,
+
+    PRIMARY KEY (`clubId`, `tagId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(20) NOT NULL DEFAULT '이름을 설정해 주세요',
+    `email` VARCHAR(30) NOT NULL,
+    `password` VARCHAR(256) NOT NULL,
+    `image` VARCHAR(191) NULL,
+    `emailVerified` DATETIME(3) NULL,
+    `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    UNIQUE INDEX `User_username_key`(`username`),
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `JoinedClub` (
+    `userId` INTEGER UNSIGNED NOT NULL,
+    `clubId` INTEGER UNSIGNED NOT NULL,
+    `joinedAt` DATE NOT NULL,
+    `isLeader` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`userId`, `clubId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER UNSIGNED NOT NULL,
+    `content` TEXT NOT NULL,
+    `createAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updateAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `isNotice` BOOLEAN NOT NULL DEFAULT false,
+    `reportCount` INTEGER UNSIGNED NOT NULL DEFAULT 0,
+    `view` INTEGER UNSIGNED NOT NULL DEFAULT 0,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `VerifyingEmail` (
+    `email` VARCHAR(30) NOT NULL,
+    `session` VARCHAR(191) NOT NULL,
+    `code` CHAR(6) NOT NULL,
+    `verifiedDone` BOOLEAN NOT NULL,
+
+    PRIMARY KEY (`email`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Account` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` INTEGER UNSIGNED NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `provider` VARCHAR(191) NOT NULL,
+    `providerAccountId` VARCHAR(191) NOT NULL,
+    `refresh_token` TEXT NULL,
+    `access_token` TEXT NULL,
+    `expires_at` INTEGER NULL,
+    `token_type` VARCHAR(191) NULL,
+    `scope` VARCHAR(191) NULL,
+    `id_token` TEXT NULL,
+    `session_state` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `Account_provider_providerAccountId_key`(`provider`, `providerAccountId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` VARCHAR(191) NOT NULL,
+    `sessionToken` VARCHAR(191) NOT NULL,
+    `userId` INTEGER UNSIGNED NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Session_sessionToken_key`(`sessionToken`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `VerificationToken` (
+    `identifier` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `VerificationToken_token_key`(`token`),
+    UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `ClubTag` ADD CONSTRAINT `ClubTag_clubId_fkey` FOREIGN KEY (`clubId`) REFERENCES `ClubList`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ClubTag` ADD CONSTRAINT `ClubTag_tagId_fkey` FOREIGN KEY (`tagId`) REFERENCES `TagList`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `JoinedClub` ADD CONSTRAINT `JoinedClub_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `JoinedClub` ADD CONSTRAINT `JoinedClub_clubId_fkey` FOREIGN KEY (`clubId`) REFERENCES `ClubList`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Account` ADD CONSTRAINT `Account_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Session` ADD CONSTRAINT `Session_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
