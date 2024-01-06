b5144860e3ba558884d8e98f91dfb59edc892170
ggomabbang <badawa7@naver.com>
2024-01-06 13:00:21 +0900

    이메일 인증 api 일부 구현

--- app/api/auth/email/[token]/route.js
+++ app/api/auth/email/[token]/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { redirect } from "next/navigation";
 import prisma from "@/prisma/prisma";
 
 export async function GET(request, params) {
@@ -12,10 +13,7 @@ export async function GET(request, params) {
     });
 
     // 토큰이 데이터베이스에 존재하지 않음 
-    if (email.length === 0) {
-
-    }
-
+    
     // 만약 토큰이 중복되어 존재한다면?
 
 
@@ -24,5 +22,5 @@ export async function GET(request, params) {
     
     // 정상적으로 인증 처리
 
-
+    redirect('http://localhost:3000/');
 }
