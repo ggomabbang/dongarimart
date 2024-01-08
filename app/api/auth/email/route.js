6774e3f02ca3b2663f62e38f95f747c490e00006
ggomabbang <badawa7@naver.com>
2024-01-08 21:11:23 +0900

    이메일 인증 이메일 전송 api 작성 완료

--- app/api/auth/email/route.js
+++ app/api/auth/email/route.js
@@ -51,8 +51,9 @@ export async function POST(request) {
 
     // 인증 토큰 생성
     const { randomBytes } = await import('node:crypto');
-    const token = randomBytes(256).toString('hex');
+    const token = randomBytes(100).toString('hex');
     console.log(token);
+    console.log(token.length);
 
     // 토큰 재발급인지 확인 
     const oldEmail = await prisma.VerifyingEmail.findUnique({
@@ -60,35 +61,50 @@ export async function POST(request) {
             email: email,
         },
         select: {
-            // 만료기한 확인
-            
+            tokencreated: true,    
+            verifiedDone: true,       
         }
     });
 
     if (oldEmail !== null) {
         // 만료기한 확인
+        const dateExpire = oldEmail.tokencreated;
+        const timeNow = new Date();
+        dateExpire.setDate(dateExpire.getDate()+1);
+        
         // 만료 안되었으면 종료
+        if (dateExpire < timeNow) {
+            return new Response(null, {
+                status: 204
+            });
+        }
 
+        // 이미 인증된 이메일이면 종료
+        if (oldEmail.verifiedDone === true) {
+            return new Response(null, {
+                status: 204
+            });
+        }
 
-        // 만료 되었으면 update
+        // 만료 되었고 아직 인증되지 않은 이메일이면 update
         const updateEmail = await prisma.VerifyingEmail.update({
             where: {
                 email: email,
             },
             data: {
-
+                token: token,
             },
         });
     }
-    
-    // 데이터베이스에 저장
-    const newEmail = await prisma.VerifyingEmail.create({
-        data: {
-            email: email,
-            token: token,
-            // 만료기한 추가
-        },
-    });
+    else {
+        // 새로운 이메일인 경우 데이터베이스에 저장
+        const newEmail = await prisma.VerifyingEmail.create({
+            data: {
+                email: email,
+                token: token,
+            },
+        });
+    }
     
     // 이메일 전송 객체 생성
     const transporter = nodeMailer.createTransport({
@@ -99,7 +115,7 @@ export async function POST(request) {
     const mailOptions = {
         to: email,
         subject: 'Wave 가입 인증 메일',
-        html: '<h1>인증링크를 클릭하세요</h1>' + "<a>" + token + '</a>'
+        html: '<h1>인증링크를 클릭하세요</h1>' + "<a href=\"http://localhost:3000/api/auth/email/" + token + "\">이메일 인증 링크 </a>"
     };
 
     await transporter.sendMail(mailOptions);
