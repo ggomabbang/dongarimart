4f3961cc3620329c5924dce6b7fad66bfde61ee0
DongHoon Lee <67902252+bluelemon61@users.noreply.github.com>
2024-01-14 19:10:32 +0900

    login, signup page 수정
    + login page 자잘한 수정
    + find page 입력 조건 검사 추가
    + find page '가입하기' 버튼 기능 추가

--- app/login/signup/page.js
+++ app/login/signup/page.js
@@ -4,25 +4,54 @@ import Styles from './SignUp.module.css';
 import { useState, useEffect } from 'react';
 
 export default function SignUp() {
+  const [nameCheck, setNameCheck] = useState(false);
+  const [nameUnduplicated, setNameUnduplicated] = useState(false);
   const [pwStyle, setPwStyle] = useState({
     1: false,
     2: false,
     3: false,
   });
   const [pwCheck, setPwCheck] = useState(false);
+  const [emailCheck, setEmailCheck] = useState(false);
+  const [emailUnduplicated, setEmailUnduplicated] = useState(false);
 
+  const submitHandler = (e) => {
+    const checks = [nameCheck, nameUnduplicated, pwCheck, emailCheck, emailUnduplicated];
+    for (const check in checks) {
+      if (!checks[check]) return alert('가입 양식이 잘못되었습니다.');
+    }
+    for (const check in pwStyle) {
+      if (!pwStyle[check]) return alert('가입 양식이 잘못되었습니다.');
+    }
+
+    alert('API 호출');
+  }
   return (
     <div className={Styles.Panel}>
       <div className={Styles.Input}>
         <div className={Styles.HorizonBox}>
-          <p className={Styles.Left}>ID</p>
+          <p className={Styles.Left}>닉네임</p>
           <div className={Styles.Right}>
             <input 
-              placeholder='ID'
+              placeholder='User Name'
               className={Styles.InputBox} 
               id='ID_box'
+              onChange={(e) => {
+                const name = e.target.value;
+                if (name.length) setNameCheck(true);
+                else setNameCheck(false);
+              }}
             />
             <button className={Styles.BlueButton}>중복확인</button>
+          </div>
+        </div>
+        <div className={Styles.HorizonBox}>
+          <p className={Styles.Left}></p>
+          <div className={Styles.Right}>
+            <ul>
+              <li id={'name_check_1'} style={{display: nameCheck ? 'none' : 'list-item'}}>닉네임을 입력해 주세요.</li>
+              <li id={'name_check_2'} style={{display: nameUnduplicated ? 'none' : 'list-item'}}>중복 확인을 눌러주세요.</li>
+            </ul>
           </div>
         </div>
 
@@ -108,14 +137,37 @@ export default function SignUp() {
           <p className={Styles.Left}>이메일</p>
           <div className={Styles.Right}>
             <input 
-              placeholder='ABC123@abc.com'
+              placeholder='ABC123@pusan.ac.kr'
               className={Styles.InputBox} 
               id='email'
+              onChange={(e) => {
+                const email = e.target.value;
+                // 이메일 형식 확인
+                const emailRE = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@pusan.ac.kr");
+                let resultRE = emailRE.exec(email);
+                if (resultRE === null) {
+                  setEmailCheck(false);
+                }
+                else {
+                  setEmailCheck(true);
+                }
+              }}  
             />
+            <button className={Styles.BlueButton}>중복확인</button>
+          </div>
+        </div>
+
+        <div className={Styles.HorizonBox}>
+          <p className={Styles.Left}></p>
+          <div className={Styles.Right}>
+            <ul>
+              <li id={'email_check_1'} style={{display: emailCheck ? 'none' : 'list-item'}}>부산대학교 이메일 'ID@pusan.ac.kr'만 허용합니다.</li>
+              <li id={'email_check_2'} style={{display: emailUnduplicated ? 'none' : 'list-item'}}>중복 확인을 눌러주세요.</li>
+            </ul>
           </div>
         </div>
         
-        <button className={Styles.BlueButton}>가입하기</button>
+        <button className={Styles.BlueButton} onClick={submitHandler}>가입하기</button>
         
       </div>
     </div>
