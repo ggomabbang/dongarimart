f8dd637577659f3236b3fa8bf9a45b080242a88e
DongHoon Lee <67902252+bluelemon61@users.noreply.github.com>
2024-01-14 20:23:16 +0900

    login/signup 완료
    + css 수정
    + Form 양식 완료
    + '가입하기' 클릭 후 동작 구현 완료

--- app/login/signup/page.js
+++ app/login/signup/page.js
@@ -2,8 +2,10 @@
 
 import Styles from './SignUp.module.css';
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function SignUp() {
+  const router = useRouter();
   const [nameCheck, setNameCheck] = useState(false);
   const [nameUnduplicated, setNameUnduplicated] = useState(false);
   const [pwStyle, setPwStyle] = useState({
@@ -15,7 +17,7 @@ export default function SignUp() {
   const [emailCheck, setEmailCheck] = useState(false);
   const [emailUnduplicated, setEmailUnduplicated] = useState(false);
 
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     const checks = [nameCheck, nameUnduplicated, pwCheck, emailCheck, emailUnduplicated];
     for (const check in checks) {
       if (!checks[check]) return alert('가입 양식이 잘못되었습니다.');
@@ -24,7 +26,53 @@ export default function SignUp() {
       if (!pwStyle[check]) return alert('가입 양식이 잘못되었습니다.');
     }
 
-    alert('API 호출');
+    const username = document.getElementById('ID_box').value;
+    const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+
+    const url = "http://localhost:3000/api/users";
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        email
+      })
+    });
+    if (res.status == 201) {
+      const emailURL = 'http://localhost:3000/api/auth/email';
+      const emailRes = await fetch(emailURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email
+        })
+      });
+      if (emailRes.status == 204) {
+        alert(`${email}로 전송된 메일을 통해 이메일 인증을 진행해주세요.`)
+        return router.push('/');
+      } else if (emailRes.status == 400) {
+        alert('MY 페이지에서 이메일 확인을 다시 진행해주세요');
+        return router.push('/');
+      } else {
+        alert('Error');
+        return router.push('/');
+      }
+    } else if (res.status == 400) {
+      alert('요청 오류');
+      return router.push('/');
+    } else if (res.status == 403) {
+      alert('잘못된 가입 양식입니다.');
+      return router.push('/');
+    } else {
+      alert('Error');
+      return router.push('/');
+    }
   }
   return (
     <div className={Styles.Panel}>
@@ -40,9 +88,34 @@ export default function SignUp() {
                 const name = e.target.value;
                 if (name.length) setNameCheck(true);
                 else setNameCheck(false);
+                setNameUnduplicated(false);
               }}
             />
-            <button className={Styles.BlueButton}>중복확인</button>
+            <button
+              className={Styles.BlueButton}
+              onClick={ async (e) => {
+                const username = document.getElementById('ID_box').value;
+                const url = "http://localhost:3000/api/users";
+                const params = new URLSearchParams();
+                params.append('username', username);
+                const res = await fetch(url + '?' + params.toString(), {
+                  method: 'GET'
+                });
+                if (res.status == 200) {
+                  const body = await res.json();
+                  if (body.exist == 'true') {
+                    alert('중복된 닉네임이 존재합니다.');
+                    setNameUnduplicated(false);
+                  }
+                  else if (body.exist == 'false') {
+                    alert('닉네임 중복확인이 완료되었습니다!')
+                    setNameUnduplicated(true);
+                  }
+                }
+              }}
+            >
+              중복확인
+            </button>
           </div>
         </div>
         <div className={Styles.HorizonBox}>
@@ -50,7 +123,7 @@ export default function SignUp() {
           <div className={Styles.Right}>
             <ul>
               <li id={'name_check_1'} style={{display: nameCheck ? 'none' : 'list-item'}}>닉네임을 입력해 주세요.</li>
-              <li id={'name_check_2'} style={{display: nameUnduplicated ? 'none' : 'list-item'}}>중복 확인을 눌러주세요.</li>
+              <li id={'name_check_2'} style={{display: nameUnduplicated ? 'none' : 'list-item'}}>중복 확인이 필요합니다.</li>
             </ul>
           </div>
         </div>
@@ -84,8 +157,6 @@ export default function SignUp() {
                 } else {
                   newStyle[3] = false;
                 }
-
-                console.log(newStyle);
                 setPwStyle({...newStyle});
               }}
             />
@@ -153,7 +224,31 @@ export default function SignUp() {
                 }
               }}  
             />
-            <button className={Styles.BlueButton}>중복확인</button>
+            <button
+              className={Styles.BlueButton}
+              onClick={ async (e) => {
+                const email = document.getElementById('email').value;
+                const url = "http://localhost:3000/api/users";
+                const params = new URLSearchParams();
+                params.append('email', email);
+                const res = await fetch(url + '?' + params.toString(), {
+                  method: 'GET'
+                });
+                if (res.status == 200) {
+                  const body = await res.json();
+                  if (body.exist == 'true') {
+                    alert('중복된 이메일이 존재합니다.');
+                    setEmailUnduplicated(false);
+                  }
+                  else if (body.exist == 'false') {
+                    alert('이메일 중복확인이 완료되었습니다!')
+                    setEmailUnduplicated(true);
+                  }
+                }
+              }}
+            >
+              중복확인
+            </button>
           </div>
         </div>
 
@@ -162,7 +257,7 @@ export default function SignUp() {
           <div className={Styles.Right}>
             <ul>
               <li id={'email_check_1'} style={{display: emailCheck ? 'none' : 'list-item'}}>부산대학교 이메일 'ID@pusan.ac.kr'만 허용합니다.</li>
-              <li id={'email_check_2'} style={{display: emailUnduplicated ? 'none' : 'list-item'}}>중복 확인을 눌러주세요.</li>
+              <li id={'email_check_2'} style={{display: emailUnduplicated ? 'none' : 'list-item'}}>중복 확인이 필요합니다.</li>
             </ul>
           </div>
         </div>
