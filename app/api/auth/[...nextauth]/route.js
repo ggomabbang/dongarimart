a9fae47864601945a6d2d133fdda9c41afc35898
ggomabbang <badawa7@naver.com>
2024-01-15 17:24:27 +0900

    Credentials 로그인 임시 작성

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,14 @@
 import NextAuth from "next-auth"
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import client from "../../../../prisma/prisma";
 import "dotenv/config";
 
 const handler = NextAuth({
-  adapter: PrismaAdapter(client),
+  //adapter: PrismaAdapter(client),
   providers: [
+    
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
@@ -18,6 +20,35 @@ const handler = NextAuth({
         }
       }
     }),
+    
+    CredentialsProvider({
+      name: 'email',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' }
+      },
+
+      async authorize(credentials, req) {
+        const res = await fetch(`${process.env.AUTH_URL}/api/auth/login`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: credentials?.email,
+            password: credentials?.password
+          }),
+        })
+
+        const user = await res.json();
+        console.log(user);
+
+        if (user) {
+          return user;
+        }
+        return null;
+      }
+    })
   ],
 });
 
