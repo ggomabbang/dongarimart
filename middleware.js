a7c9e4d4ee73fb058e471d3adeb4ea51491f3afd
ggomabbang <badawa7@naver.com>
2024-01-22 15:27:25 +0900

    middleware 추가

---
+++ middleware.js
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(request) {
+    console.log("middleware");
+
+    const token = await getToken({ req: request });
+    const url = request.nextUrl.pathname;
+
+    // 로그인했을 때 로그인 / 회원가입 페이지 접근
+    if (url.startsWith('/login')) {
+        console.log("login page");
+        if (token) {
+            return NextResponse.redirect(new URL("/", request.url));
+        }
+    }
+
+    // 로그인하지 않았을 때 My 페이지 접근
+    if (url.startsWith('/my') || url.startsWith('/register') || url.startsWith('/recruit')) {
+        console.log('my page');
+        if (token === null) {
+            return NextResponse.redirect(new URL("/login", request.url));
+        }
+        if (token.userRole !== 'user' && token.userRole !== 'admin') {
+            return NextResponse.redirect(new URL("/login", request.url));
+        }
+    }
+}
+
+export const config = {
+    matcher: ['/login/:path*', '/my', '/register', '/recruit']
+}
