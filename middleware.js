14541f9220fcd155bd1ea3879e03388256198ab4
Sunny <73125851+ggomabbang@users.noreply.github.com>
2024-02-03 15:17:20 +0900

    middleware 수정

--- middleware.js
+++ middleware.js
@@ -28,5 +28,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-    matcher: ['/login/:path*', '/my', '/register', '/recruit']
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 }
