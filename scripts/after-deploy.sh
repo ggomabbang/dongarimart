99e76254b2d66a640eb701c9c5d2902f6621258e
Sunny <73125851+ggomabbang@users.noreply.github.com>
2024-02-13 16:27:34 +0900

    command not found 문제 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
+
 REPOSITORY=/home/ec2-user/build
 
 cd $REPOSITORY
