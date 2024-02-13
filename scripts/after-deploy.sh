4ed29a61b60971ebb250d72ed834d5e23c755e4d
Sunny <73125851+ggomabbang@users.noreply.github.com>
2024-02-13 16:15:22 +0900

    codedeploy script 코드 수정 2차

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -3,6 +3,6 @@ REPOSITORY=/home/ec2-user/build
 
 cd $REPOSITORY
 
-/usr/bin/yarn
-/usr/bin/yarn migrate
-/usr/bin/pm2 start yarn --name "wave" -- start
+yarn
+yarn migrate
+pm2 start yarn --name "wave" -- start
