9dc6ebe570a33083eb77f68e8664abfe7dea5b41
kyoutae1234 <73815569+kyoutae1234@users.noreply.github.com>
2024-02-19 14:49:19 +0900

    DELETE clubs, posts, recruit 추가

---
+++ app/api/admin/recruit/[id]/route.js
@@ -0,0 +1,85 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/auth";
+import client from "../../../../../prisma/prisma";
+import { Prisma } from '@prisma/client'
+
+export async function DELETE(request) {
+  const id = parseInt(request.url.slice(request.url.lastIndexOf('/') + 1));
+
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({
+      message: "유효하지 않은 토큰입니다."
+    }, {
+      status: 401,
+    });
+  }
+
+  if (session.userRole !== 'admin') {
+    return NextResponse.json({
+      message: "등록 권한이 없는 클라이언트입니다."
+    }, {
+      status: 403,
+    });
+  }
+  
+  if(isNaN(id)) {
+    return NextResponse.json({
+      parameter: "id",
+      message: "int 형식이 아닌 ID 값입니다."
+    }, {
+      status: 400,
+    });
+  }
+
+  const myPost = await client.Post.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      userId: true,
+      clubId: true,
+      isRecruit: true,
+    }
+  });
+
+  if (!myPost || !myPost.isRecruit) {
+    return new Response(null, {
+      status: 204,
+    });
+  }
+
+  try {
+    const result = await client.Post.delete({
+      where: {
+        id,
+      }
+    });
+
+    await client.RecruitSchedule.delete({
+      where: {
+        clubId: myPost.clubId
+      }
+    })
+
+    await client.clubList.update({
+      where: {
+        id: myPost.clubId
+      },
+      data: {
+        isRecruiting: false
+      }
+    })
+
+    return NextResponse.json(result);
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({
+      message: "오류."
+    }, {
+      status: 500,
+    });
+  }
+}
