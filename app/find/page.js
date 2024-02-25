066027af5322abb6b3880358605d25f176616cf9
bluelemon61 <67902252+bluelemon61@users.noreply.github.com>
2024-02-25 16:55:29 +0900

    검색기능 추가
    + 동아리 명 검색
    + 태그 검색

--- app/find/page.js
+++ app/find/page.js
@@ -26,11 +26,17 @@ export default function find() {
   }
 
   const GetClubs = async () => {
+    const search = document.getElementById('search_').value;
+    const tag = document.getElementById('tag_').value;
+
     const urlParams = new URLSearchParams('');
+    if (search.length) urlParams.append("search", search);
+    if (tag.length) urlParams.append("tag", tag);
     urlParams.append("sortBy", SortSelected);
     if (CollegeSelected !== "all") {
       urlParams.append("college", CollegeSelected);
     }
+
     const rows = await fetch('/api/clubs?' + urlParams.toString(), {
       method: "GET"
     });
@@ -48,7 +54,18 @@ export default function find() {
     <div className={Styles.Container}>
       <h1 className={Styles.PageTitle}>동아리 찾아보기</h1>
       <div className={Styles.Horizontal_Div}>
-        <input className={Styles.SearchBar} placeholder='검색해보세요 !'/>
+        <form className={Styles.Search}>
+          <input id='search_' className={Styles.SearchBar} placeholder='동아리 이름을 검색해보세요 !'/>
+          <div className={Styles.Tag}>
+            <input id='tag_' className={Styles.TagBar} placeholder='동아리 태그'/>
+            <input
+              type='submit'
+              className={Styles.BlueButton}
+              onClick={(e) => {e.preventDefault(); GetClubs();}}
+              value='검색'
+            />
+          </div>
+        </form>
         <div className={Styles.Selector}>
           <select className={Styles.MenuFont} onChange={handleCollegeSelect} value={CollegeSelected}>
             {
@@ -73,6 +90,7 @@ export default function find() {
       </div>
       <div className={Styles.ListBox}>
         {
+          Groups.length ?
           Groups.map((club,index)=>{
             return(
               <DongariInList 
@@ -82,6 +100,10 @@ export default function find() {
               />
             );
           })
+          : 
+          <p>
+            검색 조건과 일치하는 동아리를 발견하지 못했어요! 동아리 이름이 틀리진 않았는지, 태그가 일치하는지 다시 한번 확인해 주세요!
+          </p>
         }
       </div>
     </div>
