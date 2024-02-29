9725f9d87d9e95eab87576fecde9c9abb32cc351
kyoutae1234 <73815569+kyoutae1234@users.noreply.github.com>
2024-02-29 15:27:23 +0900

    refactoring

--- app/api/clubs/route.js
+++ app/api/clubs/route.js
@@ -7,180 +7,177 @@ import { authOptions } from "@/app/lib/auth";
 import { parse } from "dotenv";
 
 export async function GET(request) {
-    const params = request.nextUrl.searchParams;
-    let sortBy = params.get("sortBy");
-    let reverse = params.get("reverse");
-    let pagination = params.get("pagination");
-    let page = params.get("page");
-    let isRecruiting = params.get("isRecruiting");
-    let tag = params.get("tag");
-    const college = params.get("college");
-    const search = params.get("search");
+  const params = request.nextUrl.searchParams;
+  let sortBy = params.get("sortBy");
+  let reverse = params.get("reverse");
+  let pagination = params.get("pagination");
+  let page = params.get("page");
+  let isRecruiting = params.get("isRecruiting");
+  let tag = params.get("tag");
+  const college = params.get("college");
+  const search = params.get("search");
 
-    if (sortBy === null) {
-      sortBy = 'registration';
-    }
-    else if(sortBy !== 'registration' && sortBy !== 'name' && sortBy !== 'deadline' && sortBy !== 'popularity') {
-      return NextResponse.json({
-          parameter: "sortby",
-          message: "지원하지 않는 형식의 값입니다."
-      }, {
-        status: 400,
-      });
-    }
-    if (reverse === null) {
-      reverse = 0;
-    }
-    if (pagination === null) {
-      pagination = 0;
-    }
-    if (page === null) {
-      page = 1;
-    }
-    if (isRecruiting === null) {
-      isRecruiting = 0;
-    }
-    if (tag !== null) {
-      tag = tag.split(',');
-    }
+  if (sortBy === null) {
+    sortBy = 'registration';
+  }
+  else if(sortBy !== 'registration' && sortBy !== 'name' && sortBy !== 'deadline' && sortBy !== 'popularity') {
+    return NextResponse.json({
+        parameter: "sortby",
+        message: "지원하지 않는 형식의 값입니다."
+    }, {
+      status: 400,
+    });
+  }
+  if (reverse === null) {
+    reverse = 0;
+  }
+  if (pagination === null) {
+    pagination = 0;
+  }
+  if (page === null) {
+    page = 1;
+  }
+  if (isRecruiting === null) {
+    isRecruiting = 0;
+  }
+  if (tag !== null) {
+    tag = tag.split(',');
+  }
 
-    reverse = parseInt(reverse);
-    pagination = parseInt(pagination);
-    page = parseInt(page);
-    isRecruiting = parseInt(isRecruiting);
+  reverse = parseInt(reverse);
+  pagination = parseInt(pagination);
+  page = parseInt(page);
+  isRecruiting = parseInt(isRecruiting);
 
-    if (isNaN(reverse)) {
-      return NextResponse.json({
-          parameter: "reverse",
-          message: "지원하지 않는 형식의 값입니다."
-      }, {
-        status: 400,
-      });
-    }
-    if (isNaN(pagination) || pagination < 0) {
-      return NextResponse.json({
-          parameter: "pagination",
-          message: "지원하지 않는 형식의 값입니다."
-      }, {
-        status: 400,
-      });
-    }
-    if (isNaN(page) || page < 1) {
-      return NextResponse.json({
-          parameter: "page",
-          message: "지원하지 않는 형식의 값입니다."
-      }, {
-        status: 400,
-      });
-    }
-    if (isNaN(isRecruiting)) {
-      return NextResponse.json({
-          parameter: "isRecruiting",
-          message: "지원하지 않는 형식의 값입니다."
-      }, {
-        status: 400,
-      });
-    }
+  if (isNaN(reverse)) {
+    return NextResponse.json({
+        parameter: "reverse",
+        message: "지원하지 않는 형식의 값입니다."
+    }, {
+      status: 400,
+    });
+  }
+  if (isNaN(pagination) || pagination < 0) {
+    return NextResponse.json({
+        parameter: "pagination",
+        message: "지원하지 않는 형식의 값입니다."
+    }, {
+      status: 400,
+    });
+  }
+  if (isNaN(page) || page < 1) {
+    return NextResponse.json({
+        parameter: "page",
+        message: "지원하지 않는 형식의 값입니다."
+    }, {
+      status: 400,
+    });
+  }
+  if (isNaN(isRecruiting)) {
+    return NextResponse.json({
+        parameter: "isRecruiting",
+        message: "지원하지 않는 형식의 값입니다."
+    }, {
+      status: 400,
+    });
+  }
 
-    const adminId = process.env.ADMIN_ID;
+  const adminId = process.env.ADMIN_ID;
 
-    let query = {
-      where: { },
-      select: {
-        id: true,
-        clubName: true,
-        classification: true,
-        oneLine: true,
-        isRecruiting: true,
-        tags: {
-          select: {
-            tagList: true,
-          }
+  let query = {
+    where: { },
+    select: {
+      id: true,
+      clubName: true,
+      classification: true,
+      oneLine: true,
+      isRecruiting: true,
+      tags: {
+        select: {
+          tagList: true,
+        }
+      },
+      members: {
+        where: {
+          isLeader: true
         },
-        members: {
-          where: {
-            isLeader: true
-          },
-          select: {
-            user: {
-              select: {
-                username: true
-              }
+        select: {
+          user: {
+            select: {
+              username: true
             }
           }
         }
       }
     }
+  }
 
-    if (isRecruiting == 1) {
+  if (isRecruiting == 1) {
+    query.where.isRecruiting = true;
+  }
+
+  const order = reverse == 1? 'desc' : 'asc';
+  switch(sortBy) {
+    case 'registration':
+      query.orderBy = [{createdAt: order}];
+      break;
+    case 'name':
+      query.orderBy = [{clubName: order}];
+      break;
+    case 'deadline':
+      query.orderBy = [{
+        schedule: {
+          recruitEnd: order
+        }
+      }];
       query.where.isRecruiting = true;
-    }
-
-    const order = reverse == 1? 'desc' : 'asc';
-    switch(sortBy) {
-      case 'registration':
-        query.orderBy = [{createdAt: order}];
-        break;
-      case 'name':
-        query.orderBy = [{clubName: order}];
-        break;
-      case 'deadline':
-        query.orderBy = [{
-          schedule: {
-            recruitEnd: order
-          }
-        }];
-        query.where.isRecruiting = true;
-        break;
-      case 'popularity':
-        const viewOrder = reverse == 0? 'desc' : 'asc';
-        query.orderBy = [{view: viewOrder}];
-        break;
-    }
-    if(pagination !== 0) {
-      query.skip = (page - 1) * pagination;
-      query.take = pagination;
-    }
-    if(tag !== null) {
-      query.where.AND = tag.map((t) => ({
-        tags: {
-          some: {
-            tagList: {
-              tagName: t
-            }
+      break;
+    case 'popularity':
+      const viewOrder = reverse == 0? 'desc' : 'asc';
+      query.orderBy = [{view: viewOrder}];
+      break;
+  }
+  if(pagination !== 0) {
+    query.skip = (page - 1) * pagination;
+    query.take = pagination;
+  }
+  if(tag !== null) {
+    query.where.AND = tag.map((t) => ({
+      tags: {
+        some: {
+          tagList: {
+            tagName: t
           }
         }
-      }));
-    }
-    if (college !== null) {
-      query.where.classification = college;
-    }
-    if (search) {
-      query.where.clubName = { contains: search };
-    }
+      }
+    }));
+  }
+  if (college !== null) {
+    query.where.classification = college;
+  }
+  if (search) {
+    query.where.clubName = { contains: search };
+  }
 
-    try {
-      const result = await client.ClubList.findMany(query);
-      const count = await client.ClubList.count(query.where);
-      const maxPage = pagination === 0 ? 1 : Math.ceil(count / pagination);
-      const clubList = result.map((res) => {
-        res.admin = res.members.length === 0 ? false : res.members[0].user.username === adminId;
-        delete res['members'];
-        return res;
-      });
-      const body = {
-        maxPage,
-        clubList
-      };
-      return NextResponse.json(body);
-    } catch (e) {
-      console.error(e);
-      return NextResponse.json({
-        message: "오류."
-      }, {
-        status: 500,
-      });
-    }
+  try {
+    const result = await client.ClubList.findMany(query);
+    const count = await client.ClubList.count(query.where);
+    const maxPage = pagination === 0 ? 1 : Math.ceil(count / pagination);
+    const clubList = result.map((res) => {
+      res.admin = res.members.length === 0 ? false : res.members[0].user.username === adminId;
+      delete res['members'];
+      return res;
+    });
+    const body = { maxPage, clubList };
+    return NextResponse.json(body);
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({
+      message: "오류."
+    }, {
+      status: 500,
+    });
+  }
 }
 
 export async function POST(request) {
